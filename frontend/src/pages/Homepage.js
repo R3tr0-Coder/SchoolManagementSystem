834c919876595d0ef8f0835c1ade4fc00a598166
R3tr0-Coder <65443632+R3tr0-Coder@users.noreply.github.com>
2024-08-14 16:01:32 +0800

    Add files via upload

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Box } from '@mui/material';
+import styled from 'styled-components';
+import Students from "../assets/students.svg";
+import { LightBlueButton } from '../components/buttonStyles';
+
+const Homepage = () => {
+    return (
+        <StyledContainer>
+            <Grid container spacing={0}>
+                <Grid item xs={12} md={6}>
+                    <img src={Students} alt="students" style={{ width: '110%' }} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <StyledPaper elevation={3}>
+                        <StyledTitle>
+                            School Management System
+                        </StyledTitle>
+                        <StyledBox>
+                            <StyledLink to="/choose">
+                                <LightBlueButton variant="contained" fullWidth>
+                                    Login
+                                </LightBlueButton>
+                            </StyledLink>
+                            <StyledText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.ss
+                            </StyledText>
+                            <StyledText>
+                                Don't have an account?{' '}
+                                <Link to="/Adminregister" style={{color:"#4275f5"}}>
+                                    Sign up
+                                </Link>
+                            </StyledText>
+                        </StyledBox>
+                    </StyledPaper>
+                </Grid>
+            </Grid>
+        </StyledContainer>
+    );
+};
+
+export default Homepage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledPaper = styled.div`
+  padding: 24px;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content:center;
+  gap: 16px;
+  padding: 24px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 3rem;
+  color: #252525;
+  font-weight: bold;
+  padding-top: 0;
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledText = styled.p`
+  margin-top: 30px;
+  margin-bottom: 30px; 
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
